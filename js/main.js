bb394757a8262bdc4dc21017ca86f0883f71ef06
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-03-06 07:51:35 +0530

    javascript
    chapter 4: numbers

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Numbers and methods
+// An integer is a whole number***
+const myNumber = 100;
+/* A number with decimal point is float which 
+references 'floating point' */
+const myFloat = 100.0;
+/* in JavaScript both integers and floats will comes 
+under Number data type**** */
+const myString = "100";
+/* if you enter a string while typecasting it will
+pass NaN to the console meaning Not a Number**** NaN**** */
+console.log(Number(false));
+/* if you tryna pass boolean while typecasting it will
+pass 1 for true and 0 for false to the console***
+**** REMEMBER in JavaScript 0 means false****** */
+
+
+
+
+
+
+
+
+
+
+
